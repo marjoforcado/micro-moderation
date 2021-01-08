5af5cbd5e65e28c8e14c4319860cbb1a0a574d01
Marjo Forcado <marjoforcado@gmail.com>
2021-01-08 15:30:18 +0800

    Add comment moderated

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.post('/events', async (req, res) => {
       const status = content.includes('orange') ? 'rejected' : 'approved';
 
       await axios.post('http://localhost:4005/events', {
+        type: 'CommentModerated',
         data: {
           id,
           postId,
