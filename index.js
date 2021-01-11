3aba3dee57d4a851de70bde294843c3463fd05f4
Marjo Forcado <marjoforcado@gmail.com>
2021-01-11 15:01:14 +0800

    Add k8 connection

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/events', async (req, res) => {
 
       const status = content.includes('orange') ? 'rejected' : 'approved';
 
-      await axios.post('http://localhost:4005/events', {
+      await axios.post('http://event-broker-srv:4005/events', {
         type: 'CommentModerated',
         data: {
           id,
